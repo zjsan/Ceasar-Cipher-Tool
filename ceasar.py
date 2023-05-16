ef19d63e12f24809dfaf9926afd50ebcdb9d6ebb
Zainal Santos <121415185+zjsan@users.noreply.github.com>
2023-05-16 11:07:51 +0800

    Create ceasar.py

---
+++ ceasar.py
@@ -0,0 +1,73 @@
+
+#cipher_text = (text, shift_key) mod 26 -> 26 characters in English Alphabet
+def encrypt(text, shift):
+
+    encrypted_text = " "
+    
+    #iterating through every character in the input string
+    for i in range(len(text)):
+        char = text[i]
+
+        #if the text is character is uppercase
+        if char.isupper():
+            #convert the character into its equivalent integer value
+            #appply shift to the individual character
+            #upper case letters in ascii starts in 65
+            # A -> 65
+            encrypted_text = encrypted_text + chr((ord(char) + shift - 65) % 26 + 65) 
+    
+        #lower case characters
+        #lower case letters in ascii starts in 97
+        # a -> 97
+        else:
+            encrypted_text = encrypted_text + chr((ord(char) + shift - 97) % 26 + 97) 
+
+    return encrypted_text
+
+def decrypt(text,shift):
+
+    decrypted_text = " "
+    #iterating through every character in the input string
+    for i in range(len(text)):
+        char = text[i]
+
+        #if the text is character is uppercase
+        if char.isupper():
+            #convert the character into its equivalent integer value
+            #appply shift to the individual character
+            #upper case letters in ascii starts in 65
+            # A -> 65
+            decrypted_text = decrypted_text + chr((ord(char) - shift - 65) % 26 + 65) 
+    
+        #lower case characters
+        #lower case letters in ascii starts in 97
+        # a -> 97
+        else:
+            decrypted_text = decrypted_text + chr((ord(char) - shift - 97) % 26 + 97) 
+
+    return decrypted_text
+
+def main():
+    
+    print("WELCOME TO CEASAR CYPHER TOOL")
+    print("SELECT THE FOLLOWING OPTIONS TO START")
+    print("[1] = ENCRYPT")
+    print("[2] = DECRYPT")
+    print("[3] = ATTACH FILE")
+    option = input()
+    
+    if option == '1':
+        text = input("Enter the string: ")
+        shift = int(input("Enter the shift value: "))
+        encrypted = encrypt(text, shift)
+        print(encrypted)
+    elif option == '2':
+        text = input("Enter the string: ")
+        shift = int(input("Enter the shift value: "))
+        decrypted = decrypt(text, shift)
+        print(decrypted)
+    elif option == '3':
+        print("hey")
+    else:
+        print("Improper Input")
+main()
